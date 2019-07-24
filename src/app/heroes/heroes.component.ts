35ba631397bf7e5dbb2c168592021f5318805c02
Milena Cabrera Patiño <mcabrerapatino@gmail.com>
2019-07-24 18:20:45 -0500

    Adding changes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  constructor() {}
+  ngOnInit() {}
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas'},
+  { id: 15, name: 'Magneta' },
+  { id: 15, name: 'RuberMan' },
+  { id: 16, name: 'Dynama' },
+  { id: 17, name: 'Dr IQ' },
+  { id: 18, name: 'Magma' },
+  { id: 19, name: 'Tornado' },
+];
